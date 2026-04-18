672357587d6851cfe074fefca2a588b9834815c4
mythstreetstore-svg <mythstreetstore@gmail.com>
2026-04-18 16:14:20 +0530

    final fix build

--- next.config.ts
+++ next.config.ts
@@ -3,17 +3,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "**",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
